15a788c5ce619040fee4c538250fba2188df0f22
Marc Fournier <marc.fournier@camptocamp.com>
2009-07-13 16:21:16 +0200

    nfs::base: added support for redhat + added dependencies

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -1,17 +1,48 @@
 class nfs::base {
-  package { ["nfs-common", "portmap"]:
+
+  package { "portmap":
     ensure => present,
   }
 
-  service { "nfs-common":
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-  }
+  case $operatingsystem {
 
-  service { "portmap":
-    ensure    => running,
-    enable    => true,
-    hasstatus => false,
+    Debian: {
+      package { "nfs-common":
+        ensure => present,
+      }
+
+      service { "nfs-common":
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        require   => Package["nfs-common"],
+      }
+
+      service { "portmap":
+        ensure    => running,
+        enable    => true,
+        hasstatus => false,
+        require   => Package["portmap"],
+      }
+    }
+
+    RedHat: {
+      package { "nfs-utils":
+        ensure => present,
+      }
+
+      service { "netfs":
+        enable  => true,
+        require => [Service["portmap"], Service["nfslock"]],
+      }
+
+      service { ["portmap", "nfslock"]:
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        require   => [Package["portmap"], Package["nfs-utils"]],
+      }
+    }
+
   }
 }
