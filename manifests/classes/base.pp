efebd9002601682fa8aeef206f7024e8404a3d5c
Marc Fournier <marc.fournier@camptocamp.com>
2009-07-13 15:43:17 +0200

    nfs::base added missing service resources

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -2,4 +2,16 @@ class nfs::base {
   package { ["nfs-common", "portmap"]:
     ensure => present,
   }
+
+  service { "nfs-common":
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+  }
+
+  service { "portmap":
+    ensure    => running,
+    enable    => true,
+    hasstatus => false,
+  }
 }
