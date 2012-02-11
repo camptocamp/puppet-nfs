056dae02cd937776ab6d7d7c7d0f465f53818814
Mikael Fridh <mfridh@marinsoftware.com>
2012-02-11 04:58:26 +0100

    redhat server support

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,8 @@
 class nfs::server {
+  include concat::setup
   case $operatingsystem {
     Debian,Ubuntu:  { include nfs::server::debian}
+    RedHat,CentOS,scientific,oel:  { include nfs::server::redhat}
     default:        { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 }
