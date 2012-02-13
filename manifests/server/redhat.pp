dbefaf87a25c24903ce1548e1fe1075a1b821fa9
Mikael Fridh <mfridh@marinsoftware.com>
2012-02-13 16:46:18 +0100

    missing Package dependency

--- manifests/server/redhat.pp
+++ manifests/server/redhat.pp
@@ -3,13 +3,14 @@ class nfs::server::redhat inherits nfs::client::redhat {
   exec {"reload_nfs_srv":
     command     => "/etc/init.d/nfs reload",
     refreshonly => true,
-    require     => Package["nfs-utils"]
+    require     => Package["nfs-utils"],
   }
 
   service {"nfs":
     ensure     => running,
     enable     => true,
     hasrestart => true,
+    require    => Package["nfs-utils"],
   }
 
 }
