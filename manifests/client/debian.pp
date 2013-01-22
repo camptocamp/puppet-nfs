435e7970654fd8ae91cfc58d73970bacc5c09907
spali <spali@spali.ch>
2013-01-22 12:05:52 +0100

    handle Ubuntu which still use portmap instead of rpcbind
    currently Ubuntu keep "portmap" as service name, event the portmap package is replaced with rpcbind.

--- manifests/client/debian.pp
+++ manifests/client/debian.pp
@@ -1,13 +1,20 @@
 class nfs::client::debian inherits nfs::base {
 
-  case $lsbdistcodename {
-    squeeze:  {
+  case $operatingsystem {
+    Debian: {
+      case $lsbdistcodename {
+        squeeze:  {
+          $rpcbind_service_name = "portmap"
+          $rpcbind_package_name = "portmap" 
+        }
+        default:  {
+          $rpcbind_service_name = "rpcbind"
+          $rpcbind_package_name = "rpcbind" 
+        }
+      }
+    default:  {
       $rpcbind_service_name = "portmap"
       $rpcbind_package_name = "portmap" 
-    }
-    default:  {
-      $rpcbind_service_name = "rpcbind"
-      $rpcbind_package_name = "rpcbind" 
     }
   }
   
