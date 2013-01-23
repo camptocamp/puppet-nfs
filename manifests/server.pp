80814b1d934d2958c42effdbafc5f65a38c96a3a
Nikola Petrov <nikolavp@gmail.com>
2013-01-23 15:41:00 +0200

    Provide support for ubuntu.
    On ubuntu there is no nfs-common service so we can extend from the
    debian support and override this.

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,7 +2,8 @@ class nfs::server(
   $service_enable = true,
 ) {
   case $operatingsystem {
-    Debian,Ubuntu:  { include nfs::server::debian}
+    Ubuntu:         { include nfs::server::ubuntu}
+    Debian:         { include nfs::server::debian}
     default:        { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 }
