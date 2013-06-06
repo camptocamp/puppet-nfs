a1f2e98cc33a54efa6a78dea5858bba2a611537b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-06-06 12:35:39 +0000

    some lint and allow to set share order using $name

--- manifests/export.pp
+++ manifests/export.pp
@@ -1,18 +1,20 @@
-define nfs::export ($ensure=present,
-                    $share,
-                    $options="",
-                    $guest) {
+define nfs::export (
+  $guest,
+  $share,
+  $ensure=present,
+  $options='',
+) {
 
   $concatshare = regsubst($share, '/', '-', 'G')
   $concatguest = regsubst($guest, '/','-', 'G')
- 
-  if $options == "" {
+
+  if $options == '' {
     $content = "${share}     ${guest}\n"
   } else {
     $content = "${share}     ${guest}($options)\n"
   }
-  
-  concat::fragment {"${concatshare}-on-${concatguest}":
+
+  concat::fragment {"${name}-${concatshare}-on-${concatguest}":
     ensure  => $ensure,
     content => $content,
     target  => '/etc/exports',
