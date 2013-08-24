5d0ef134fd9412f25589c6dba318ca45aeee34df
Mateusz Zawisza <mateusz@applicake.com>
2013-08-24 18:25:15 +0200

    make Concat[/etc/exports] a virtual resource
    This is fix for error, that occurs when nfs server is created before
    clients. Concat fails if fragments dir is empty:
    
      /var/lib/puppet/concat/bin/concatfragments.sh -o
      /var/lib/puppet/concat/_etc_exports/fragments.concat.out -d
      /var/lib/puppet/concat/_etc_exports returned 1 instead of one of [0]
    
    Making Concat[/etc/exports] a virtual resource and realizing it in
    nfs::export, we make sure that concat is not triggered if there is
    nothing to concatenate.

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -18,7 +18,7 @@ class nfs::server::debian inherits nfs::client::debian {
     pattern => "nfsd"
   }
 
-  concat {'/etc/exports':
+  @concat {'/etc/exports':
     owner  => root,
     group  => root,
     mode   => '0644',
