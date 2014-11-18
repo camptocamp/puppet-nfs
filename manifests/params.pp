6774b645c69ca3b2414f44bda629a23d844f9c86
Simon Séhier <simon.sehier@camptocamp.com>
2014-11-18 15:53:39 +0100

    set portmap service name for ubuntu. should fix#24

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,11 @@
+# Set default parameters based on distribution release
 class nfs::params {
   $portmap = $::osfamily? {
     Debian    => $::lsbdistcodename? {
       Wheezy  => 'rpcbind',
       default => 'portmap',
     },
-    default   => 'portmap'
+    Ubuntu   => 'rpcbind',
+    default  => 'portmap'
   }
 }
