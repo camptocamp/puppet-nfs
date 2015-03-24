68aa319ab78808e90aa459dc9e0842276be37baa
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2015-03-24 13:21:12 +0000

    Explicitly specify portmap "service" and "package" names as they are not always e.g. on Ubuntu precise and lucid.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,22 @@
 # Set default parameters based on distribution release
 class nfs::params {
-  $portmap = $::operatingsystem? {
+  $package::portmap = $::operatingsystem? {
     'Debian' => $::lsbdistcodename? {
       'Wheezy' => 'rpcbind',
       default  => 'portmap',
     },
     'Ubuntu' => 'rpcbind',
+    default  => 'portmap'
+  }
+  $service::portmap = $::operatingsystem? {
+    'Debian' => $::lsbdistcodename? {
+      'Wheezy' => 'rpcbind',
+      default  => 'portmap',
+    },
+    'Ubuntu' => $::lsbdistcodename? {
+      'trusty' => 'rpcbind',
+      default  => 'portmap',
+    },
     default  => 'portmap'
   }
 }
