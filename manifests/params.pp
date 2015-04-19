6c060f562421af4a519162e2631e4ef7753476d7
intrigeri <intrigeri@boum.org>
2015-04-19 07:17:17 +0000

    Set $nfs::params::portmap to rpcbind on Debian Wheezy and newer.
    We did that for Wheezy already. Let's do that for anything newer as well.
    Without this change, $nfs::params::portmap is erroneously set to 'portmap'
    on Debian 8 (Jessie).

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,10 @@
 # Set default parameters based on distribution release
 class nfs::params {
   $portmap = $::operatingsystem? {
-    'Debian' => $::lsbdistcodename? {
-      'Wheezy' => 'rpcbind',
-      default  => 'portmap',
+    'Debian' => versioncmp($::operatingsystemmajrelease, 7)? {
+      1    => 'rpcbind',
+      0    => 'rpcbind',
+      '-1' => 'portmap',
     },
     'Ubuntu' => 'rpcbind',
     default  => 'portmap'
