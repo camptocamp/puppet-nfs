2f167f9f7545c53982fa7edf1386c80dc617ca9b
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2015-04-22 17:08:49 +0200

    Fixed a problem on Ubuntu when calling both server and client classes

--- manifests/server/ubuntu.pp
+++ manifests/server/ubuntu.pp
@@ -1,7 +1,4 @@
 # Specific settings for server on Ubuntu
 class nfs::server::ubuntu inherits nfs::server::debian {
-  Service['nfs-common'] {
-    name => 'statd',
-  }
   Package[$nfs::params::portmap] { }
 }
