1e459275c98c55e2d973e6613aa01d145de4d630
Christina Kyriakidou <ckyriaki@redhat.com>
2015-07-15 15:25:08 +0200

    Adding RHEL 7 support

--- manifests/server/redhat.pp
+++ manifests/server/redhat.pp
@@ -6,11 +6,25 @@
 #
 class nfs::server::redhat inherits nfs::client::redhat {
 
+  $servicename = $::operatingsystemmajrelease ? {
+    '7'     => 'nfs-server',
+    default => 'nfs'
+
+  }
+
   service{ 'nfs':
     ensure    => running,
+    name      => $servicename,
     enable    => true,
     hasstatus => true,
     require   => Package[ 'nfs-utils' ],
+  }
+
+  @concat {'/etc/exports':
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    notify => Service['nfs'],
   }
 
 }
