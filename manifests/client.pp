b4f92dca1f06c05d583f47a7fed43032be8003b2
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2016-08-16 09:09:05 +0200

    Refactor client/debian and add Xenial support
    Merged the Ubuntu + Debian classes and used params to set differences.

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,11 +3,10 @@
 # This class configures NFS on client side
 #
 class nfs::client {
-  case $::operatingsystem {
-    'Ubuntu':            { include ::nfs::client::ubuntu}
-    'Debian':            { include ::nfs::client::debian}
-    /^(RedHat|CentOS|OracleLinux)$/: { include ::nfs::client::redhat}
-    default:        { notice "Unsupported operatingsystem ${::operatingsystem}" }
+  case $::osfamily {
+    'Debian': { include ::nfs::client::debian}
+    'RedHat': { include ::nfs::client::redhat}
+    default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
   }
 }
 
